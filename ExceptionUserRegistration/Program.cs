84e61de8474bc682e96c2452fb890d716a5d2b30
navitha <navithareddy112@gmail.com>
2022-09-26 18:09:48 +0530

    [navitha] UC5PasswordRule1

--- ExceptionUserRegistration/Program.cs
+++ ExceptionUserRegistration/Program.cs
@@ -5,10 +5,12 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("choose a option 1.valid \t 2. lastname \t 3.Email \t 4.mobilenumber");
+            Console.WriteLine("choose a option 1.valid \t 2. lastname \t 3.Email \t 4.mobilenumber \t 5.rule1");
             Console.WriteLine("UC1Valid first name");
             Console.WriteLine("UC2 Valid last name");
             Console.WriteLine("UC3 Email");
+            Console.WriteLine("UC4 mobile number");
+            Console.WriteLine("UC5 rule1 password");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -24,6 +26,9 @@
                     break;
                 case 4:
                     UC4MobileNumber.ValidMobileNumber();
+                    break;
+                case 5:
+                    UC5PasswordRule1.ValidPasswordRule1();
                     break;
                 default:
                     Console.WriteLine("enter wrong choice");
---
+++ ExceptionUserRegistration/UC5PasswordRule1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExceptionUserRegistration
+{
+    public class UC5PasswordRule1
+    {
+        public static void ValidPasswordRule1()
+        {
+            try
+            {
+                Console.WriteLine("Enter password");
+                 Console.ReadLine();
+                string password = Console.ReadLine();
+                var hasLowerChar = new Regex("^(?=.*[a-z]).{8,}$");
+                if (!hasLowerChar.IsMatch(password))
+                {
+                    throw (new Exception("password is not valid .... it must contain 8 characters"));
+
+                    Console.WriteLine("password should be minimum 8 characters");
+                }
+                else
+                {
+                    Console.WriteLine("password is " + password);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
