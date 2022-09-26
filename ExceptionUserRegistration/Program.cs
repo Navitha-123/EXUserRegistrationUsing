e3e1259e6ca29fa0c5ea4c53e5154cb5edf9c68c
navitha <navithareddy112@gmail.com>
2022-09-26 18:31:02 +0530

    [navitha] ValidSampleEmail

--- ExceptionUserRegistration/Program.cs
+++ ExceptionUserRegistration/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("choose a option 1.valid \t 2. lastname \t 3.Email \t 4.mobilenumber \t 5.rule1 \t 6.rule2 \t 7.rule 3");
+            Console.WriteLine("choose a option 1.valid \t 2. lastname \t 3.Email \t 4.mobilenumber \t 5.rule1 \t 6.rule2 \t 7.rule \t 9.Emailsample");
             Console.WriteLine("UC1Valid first name");
             Console.WriteLine("UC2 Valid last name");
             Console.WriteLine("UC3 Email");
@@ -13,6 +13,7 @@
             Console.WriteLine("UC5 rule1 password");
             Console.WriteLine("UC6 rule2 password");
             Console.WriteLine("UC7 Rule3");
+            Console.WriteLine("UC9 valid samples");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -37,6 +38,9 @@
                     break;
                 case 7:
                     UC7PasswordRule3.ValidPasswordRule3();
+                    break;
+                case 9:
+                    UC9EmailSamples.ValidSampleEmail();
                     break;
                 default:
                     Console.WriteLine("enter wrong choice");
