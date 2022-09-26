0d7a9c9a70bc6ffad27f31debb47b14753560188
navitha <navithareddy112@gmail.com>
2022-09-26 18:25:40 +0530

    [navitha] UC7PasswordRule3

--- ExceptionUserRegistration/Program.cs
+++ ExceptionUserRegistration/Program.cs
@@ -5,13 +5,14 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("choose a option 1.valid \t 2. lastname \t 3.Email \t 4.mobilenumber \t 5.rule1 \t 6.rule2");
+            Console.WriteLine("choose a option 1.valid \t 2. lastname \t 3.Email \t 4.mobilenumber \t 5.rule1 \t 6.rule2 \t 7.rule 3");
             Console.WriteLine("UC1Valid first name");
             Console.WriteLine("UC2 Valid last name");
             Console.WriteLine("UC3 Email");
             Console.WriteLine("UC4 mobile number");
             Console.WriteLine("UC5 rule1 password");
             Console.WriteLine("UC6 rule2 password");
+            Console.WriteLine("UC7 Rule3");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -33,6 +34,9 @@
                     break;
                 case 6:
                     UC6PasswordRule2.ValidPasswordRule2();
+                    break;
+                case 7:
+                    UC7PasswordRule3.ValidPasswordRule3();
                     break;
                 default:
                     Console.WriteLine("enter wrong choice");
